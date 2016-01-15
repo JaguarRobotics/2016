2d8e3dd075adc610fc123615b3b52d791eacffef
charlie <charlie@10.9.117.54>
2016-01-14 19:51:27 -0600

    need to add " public static final int pwmShooterMotor       = 10000000;//temporary pwm value you can change if you want   " to robot map

---
+++ src/edu/jaguarbots/stronghold/subsystems/ShooterSubsystem.java
@@ -0,0 +1,20 @@
+package edu.jaguarbots.stronghold.subsystems;
+
+import edu.jaguarbots.stronghold.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ShooterSubsystem extends Subsystem {
+    
+    private DigitalInput shooter = new DigitalInput(RobotMap.pwmShooterMotor);
+
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+

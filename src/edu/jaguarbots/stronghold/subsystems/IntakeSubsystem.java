4541b0e02c026748bf41ae860e7843e1c6d4fa07
Natha <Natha@10.9.28.28>
2016-01-14 19:47:02 -0600

    added start and stop motor functions in IntakeSubsystem

--- src/edu/jaguarbots/stronghold/subsystems/IntakeSubsystem.java
+++ src/edu/jaguarbots/stronghold/subsystems/IntakeSubsystem.java
@@ -1,15 +1,25 @@
 package edu.jaguarbots.stronghold.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.Talon;
+import edu.jaguarbots.stronghold.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  *
  */
 public class IntakeSubsystem extends Subsystem {
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    private DigitalInput intakeMotor = new DigitalInput(RobotMap.pwmIntakeMotor);
+    
+    public void startMotor()
+    {
+        intakeMotor.startLiveWindowMode();
+    }
+
+    public void stopMotor()
+    {
+        intakeMotor.stopLiveWindowMode();
+    }
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.

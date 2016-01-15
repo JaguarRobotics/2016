b5c152b93a5c4b221a11cdd719bdfc16595ab981
charlie <charlie@10.9.117.54>
2016-01-14 19:56:18 -0600

    fixed something

--- src/edu/jaguarbots/stronghold/RobotMap.java
+++ src/edu/jaguarbots/stronghold/RobotMap.java
@@ -15,4 +15,5 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+    public static final int pwmShooterMotor       = 10000000;//temporary pwm value you can change if you want   
 }

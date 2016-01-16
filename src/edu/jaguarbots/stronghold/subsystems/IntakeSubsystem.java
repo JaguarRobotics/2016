7b08502f294f77a85e4d5e37f7e437eb5b7c3cf3
Natha <Natha@10.9.117.26>
2016-01-15 20:09:03 -0600

    Changed the intake motor to a victor, because that is what the important sheet from Kyle said.

--- src/edu/jaguarbots/stronghold/subsystems/IntakeSubsystem.java
+++ src/edu/jaguarbots/stronghold/subsystems/IntakeSubsystem.java
@@ -3,22 +3,42 @@ package edu.jaguarbots.stronghold.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.jaguarbots.stronghold.RobotMap;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  *
  */
 public class IntakeSubsystem extends Subsystem {
     
-    private DigitalInput intakeMotor = new DigitalInput(RobotMap.pwmIntakeMotor);
+    private Victor intakeMotor = new Victor(RobotMap.pwmIntakeMotor);
     
-    public void startMotor()
+    private IntakeSubsystem()
     {
-        intakeMotor.startLiveWindowMode();
+        
     }
-
+    
+    /*
+     * runs the intake motor in the default direction
+     */
+    public void startMotorForward()
+    {
+        intakeMotor.set(1);
+    }
+    
+    /*
+     * runs the intake motor in the opposite direction of the default direction
+     */
+    public void startMotorBackward()
+    {
+        intakeMotor.set(-1);
+    }
+    
+    /*
+     * stops the intake motor
+     */
     public void stopMotor()
     {
-        intakeMotor.stopLiveWindowMode();
+        intakeMotor.set(0);
     }
     
     public void initDefaultCommand() {

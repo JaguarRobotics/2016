87f55566952bc68951c69e0d61fb11f963ed4350
Chris <cgreenfield01@gmail.com>
2016-02-20 11:45:34 -0600

    Changed HIGH Value to VALUE Variable in Calculations.ino

--- lights/calculations.ino
+++ lights/calculations.ino
@@ -2,31 +2,33 @@
 #include "calculations.h"
 #include "patterns.h"
 
+#define VALUE LOW
+
 CRGB calculateLED(int frame, int led) {
-  if ( digitalRead(CTRL0) == HIGH ) {
-    if ( digitalRead(CTRL1) == HIGH ) {
-      if ( digitalRead(CTRL2) == HIGH ) {
-        if ( digitalRead(CTRL3) == HIGH ) {
+  if ( digitalRead(CTRL0) == VALUE ) {
+    if ( digitalRead(CTRL1) == VALUE ) {
+      if ( digitalRead(CTRL2) == VALUE ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern0(frame, led);
         } else {
           return pattern1(frame, led);
         }
       } else {
-        if ( digitalRead(CTRL3) == HIGH ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern2(frame, led);
         } else {
           return pattern3(frame, led);
         }
       }
     } else {
-      if ( digitalRead(CTRL2) == HIGH ) {
-        if ( digitalRead(CTRL3) == HIGH ) {
+      if ( digitalRead(CTRL2) == VALUE ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern4(frame, led);
         } else {
           return pattern5(frame, led);
         }
       } else {
-        if ( digitalRead(CTRL3) == HIGH ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern6(frame, led);
         } else {
           return pattern7(frame, led);
@@ -34,29 +36,29 @@ CRGB calculateLED(int frame, int led) {
       }
     }
   } else {
-    if ( digitalRead(CTRL1) == HIGH ) {
-      if ( digitalRead(CTRL2) == HIGH ) {
-        if ( digitalRead(CTRL3) == HIGH ) {
+    if ( digitalRead(CTRL1) == VALUE ) {
+      if ( digitalRead(CTRL2) == VALUE ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern8(frame, led);
         } else {
           return pattern9(frame, led);
         }
       } else {
-        if ( digitalRead(CTRL3) == HIGH ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern10(frame, led);
         } else {
           return pattern11(frame, led);
         }
       }
     } else {
-      if ( digitalRead(CTRL2) == HIGH ) {
-        if ( digitalRead(CTRL3) == HIGH ) {
+      if ( digitalRead(CTRL2) == VALUE ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern12(frame, led);
         } else {
           return pattern13(frame, led);
         }
       } else {
-        if ( digitalRead(CTRL3) == HIGH ) {
+        if ( digitalRead(CTRL3) == VALUE ) {
           return pattern14(frame, led);
         } else {
           return pattern15(frame, led);
